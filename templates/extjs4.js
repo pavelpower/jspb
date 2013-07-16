e39947593e12b97b2edca1095640a559e296abb8
pavelpower <pavel.power@gmail.com>
2013-07-16 23:07:36 +0400

    added new regexp for mixins

--- templates/extjs4.js
+++ templates/extjs4.js
@@ -1,13 +1,13 @@
 
 function _requires (content) {
     var rgx = /(?:requires|mixins)\s*:\s*\[([^\[\]]+)\]/gim,
-        rgx_Name = /['"]([^'",]*)['"]/gim,
+        rgx_name = /['"]([^'",]*)['"]/gim,
         requires_lines = content.match(rgx),
         result = [];
 
     if (requires_lines) {
         requires_lines.forEach(function(l) {
-            var names = l.match(rgx_Name);
+            var names = l.match(rgx_name);
             if (names) {
                 result = result.concat(names.map(function(p) {
                     return p.replace(/['"]*/gim,'').trim();
@@ -20,7 +20,7 @@ function _requires (content) {
 }
 
 function _extend_requires (content) {
-    var rgx = /(?:extend|requires|mixins):\s*['"]([^'"]*)['"]/gim,
+    var rgx = /(?:extend|requires|mixins)\s*:\s*['"]([^'"]*)['"]/gim,
         requires_line = content.match(rgx);
 
     if (requires_line == null)
@@ -29,6 +29,32 @@ function _extend_requires (content) {
     return requires_line.map(function(l) {
         return l.replace(rgx, '$1');
     });
+}
+
+// for type
+/*
+ mixins: {
+ observable: 'Ext.mixin.Observable'
+ }
+ */
+function _mixmins_hash(content) {
+    var rgx = /(?:mixins)\s*:\s*(\{[^\{\}]*\})/gim,
+        rgx_name = /:\s['"]([^'"]+)['"]/gim,
+        requires_line = content.match(rgx),
+        result = [];
+
+    if (requires_line != null) {
+
+        requires_line.forEach(function(l) {
+            var names = l.match(rgx_name);
+            if (names) {
+                result = result.concat(names.map(function(p) {
+                    return p.replace(rgx_name,'$1').trim();
+                }));
+            }
+        });
+    }
+    return result;
 }
 
 module.exports = {
@@ -49,6 +75,7 @@ module.exports = {
         var blanks;
         blanks = _requires(content);
         blanks = blanks.concat(_extend_requires(content));
+        blanks = blanks.concat(_mixmins_hash(content));
         return blanks;
     }
 };
